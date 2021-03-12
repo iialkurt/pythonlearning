59df01f382782afe5f2236f9a5cf7b9d5bfebcab
iialkurt <35519430+iialkurt@users.noreply.github.com>
2021-03-13 01:08:24 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,11 @@
+cv1={"Adı :":"İsmail","Soyadı :":"ALKURT","Doğum Yılı: ":"1983","Mesleği :":"Bilişim","Mezuniyet :":"Üniversite","Özgeçmiş :":"Lorem ipsum do dolor"}
+cv2={"Adı :":"Ali","Soyadı :":"VELİ","Doğum Yılı: ":"1993","Mesleği :":"Oto","Mezuniyet :":"Lise","Özgeçmiş :":"Lorem ipsum do dolor"}
+cv3={"Adı :":"Sevig","Soyadı :":"UYSAL","Doğum Yılı: ":"1991","Mesleği :":"Pazarlama","Mezuniyet :":"Üniversite","Özgeçmiş :":"Lorem ipsum do dolor"}
+cv4={"Adı :":"Mehmet","Soyadı :":"AK","Doğum Yılı: ":"1974","Mesleği :":"Satınalma","Mezuniyet :":"Lise","Özgeçmiş :":"Lorem ipsum do dolor"}
+cv5={"Adı :":"Ahmet","Soyadı :":"KARA","Doğum Yılı: ":"1978","Mesleği :":"Personel","Mezuniyet :":"Üniversite","Özgeçmiş :":"Lorem ipsum do dolor"}
+
+print(cv1)
+print(cv2)
+print(cv3)
+print(cv4)
+print(cv5)
